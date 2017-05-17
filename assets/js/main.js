b34fa70d4ebd9d9f1e95ac6bbb2afb944ba3cbec
TanvirGalive <galivetanvir@gmail.com>
2017-05-17 18:29:43 +0600

    innitial commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        $(".homepage-slides").owlCarousel({
+            items:1,
+            dots: true,
+            nav: true,
+            autolpay: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            loop: true,
+            animateOut: 'fadeOut'
+            
+        });
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
